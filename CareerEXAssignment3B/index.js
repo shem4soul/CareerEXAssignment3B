9f8b642ebe0514ddfba6559ac5d0371018e4caf5
Shittu Emmanuel Seun <50348862+shem4soul@users.noreply.github.com>
2024-07-25 22:17:13 -1200

    Add files via upload

---
+++ CareerEXAssignment3B/index.js
@@ -0,0 +1,40 @@
+// 1.  Strings
+
+let name = "SHITTU";
+let qualification = "Msc";
+let city = "Benin City";
+let FootballClub = "Chelsea";
+let favouriteFood = "Jollof Rice";
+
+// 2. Numbers
+
+let year = 2011;
+let age = 19;
+let time  = 10.31;
+let distance = 200;
+let price = 7900;
+
+
+// 3. Booleans
+
+let isWeekend = true;
+let isLoggedIn = true
+let isActive = true;
+let isChristmas = false;
+let electionTime = true;
+
+//4. Null
+
+let television = null;
+let user = null;
+let result = null;
+let score = null;
+let data = null;
+
+// 5. Undefined
+
+let Age;
+let game;
+let phone;
+let car;
+let startTime;
